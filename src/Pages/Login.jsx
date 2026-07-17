d61b2ed57c2da9a71f804047634accfe624e7488
Priyanshu Sharma <priyanshusharma4850@gmail.com>
2026-07-17 23:29:47 +0530

    changes the login and signup pages

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,8 +3,8 @@ import { MdAccountCircle } from "react-icons/md";
 import { Link } from "react-router-dom";
 function Login() {
 	return (
-		<div className="w-full h-screen bg-blue-200 flex justify-center items-center">
-			<div className="w-[50%] h-[80%] bg-blue-500 shadow-2xl flex flex-col items-center justify-center gap-5">
+		<div className="w-full h-screen bg-blue-100 flex justify-center items-center">
+			<div className="w-[50%] h-[80%] bg-blue-300 shadow-2xl flex flex-col items-center justify-center gap-5 rounded-2xl">
 				<MdAccountCircle className="size-40"></MdAccountCircle>
 				<h1 className="text-3xl">COUSTOMER LOGIN</h1>
 				<form
